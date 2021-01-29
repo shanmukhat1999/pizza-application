45bada68c320ec63a643895e5e8ce851ce68a93b
shanmukhat1999 <57249299+shanmukhat1999@users.noreply.github.com>
2021-01-29 18:24:30 +0530

    Update views.py

--- pizza/views.py
+++ pizza/views.py
@@ -37,7 +37,7 @@ def menu(request):
         if user is None:
             return render(request,'pizza/login.html',{"msg":"wrong credentials"})
         request.session["username"]=username    
-    return redirect('menu1')
+    return redirect(menu1)
     print("b")
 
 def menu1(request):
@@ -63,11 +63,12 @@ def orders(request):
         return render(request,'pizza/index.html',{"msg":"Login first to place order"})
     item_names=request.POST["item_names"]
     username=request.POST["username"]
+    price=request.POST["price"]
     now = datetime.now()
     dt_string = now.strftime("On %d/%m/%Y At %H:%M:%S")	
     counters=counter.objects.get(pk=1)
     k=counters.count
-    order = user_order(name=username,items=item_names,order_no=k,time_ordered=dt_string)
+    order = user_order(name=username,items=item_names,order_no=k,time_ordered=dt_string,price=price)
     c=counter.objects.get(pk=1)
     c.count=k+1
     c.save()
@@ -95,16 +96,4 @@ def logout_view(request):
         return redirect('index')
     request.session.pop("username")
     return redirect("index")
-
-
-
-
-
-
-    
-
-
-
-
-
 
