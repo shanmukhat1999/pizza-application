fed550fcf3a18b8e6e1dd95f5c79417450d223db
shanmukhat1999 <57249299+shanmukhat1999@users.noreply.github.com>
2021-02-02 14:15:20 +0530

    Update admin.py

--- pizza/admin.py
+++ pizza/admin.py
@@ -3,12 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
-admin.site.register(Regular_pizza)
-admin.site.register(Sicilian_pizza)
-admin.site.register(Topping)
-admin.site.register(Sub)
-admin.site.register(Pasta)
-admin.site.register(Salad)
-admin.site.register(Dinner_platter)
-admin.site.register(user_order)
-admin.site.register(counter)
+class OrderInline(admin.TabularInline):
+    model = Order
+
+class User_orderAdmin(admin.ModelAdmin):
+    inlines = [
+        OrderInline,
+    ]
+
+admin.site.register(Item)
+admin.site.register(User_order,User_orderAdmin)
+admin.site.register(Order)
