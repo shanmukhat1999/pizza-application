3dae53958fc3cf7341b50b689e984eedda56c1db
shanmukhat1999 <shanmukhat999@gmail.com>
2022-09-24 15:55:37 +0530

    add refresh order status button

--- pizza/static/pizza/myorders.js
+++ pizza/static/pizza/myorders.js
@@ -2,4 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     if (!!window.performance && window.performance.navigation.type == 2) {
         window.location.reload(true);
     }
+
+    document.querySelector('button').onclick = () => {
+        window.location.reload(true);
+    }
 });
